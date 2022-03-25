165edcdf29c5e3cf1b7ac6b0204e98c445db15e6
blacktunes <blacktunes@foxmail.com>
2022-03-25 19:07:10 +0800

    feat: 优化组件和更新吃桃文

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="title">
-    <transition name="fade">
+    <div style="display:none">
+      <img src="./assets/images/start_hover.png" />
+      <img src="./assets/images/start_active.png" />
+      <img src="./assets/images/extra_hover.png" />
+      <img src="./assets/images/extra_active.png" />
+      <img src="./assets/images/exit_hover.png" />
+      <img src="./assets/images/exit_active.png" />
+    </div>
+    <transition name="fade" appear @after-leave="titleLeave">
       <div class="wrapper" v-if="title">
-        <img class="img" alt="title" src="./assets/hiiropara.png" />
-        <div class="button button1" @click="click"></div>
-        <div class="button button2"></div>
-        <div class="button button3"></div>
-        <div class="button button4" @click="click"></div>
-        <a class="button button5" href="https://www.bilibili.com/video/BV1GJ411x7h7"></a>
+        <img class="img" :src="bg" />
+        <div class="button start" @click="showMsg"></div>
+        <div class="button load"></div>
+        <div class="button config"></div>
+        <div class="button extra" @click="showTip" v-if="extra"></div>
+        <div class="button extra-disable" v-else></div>
+        <div class="button exit" @click="exit"></div>
         <transition name="fade">
-          <img
-            class="tip"
-            alt="tip"
-            src="./assets/tip.png"
-            v-if="show"
-            @click="hide"
-          />
+          <img class="tip" src="./assets/images/tip.png" v-if="tipShow" @click="hideTip" />
         </transition>
       </div>
     </transition>
-  </div>
-  <div class="logo-wrapper">
-    <transition name="logo" @after-enter="playVoice" @after-leave="showTitle">
-      <img class="logo" alt="logo" src="./assets/logo.png" v-if="logo" />
+    <transition name="fade" @after-leave="messageLeave">
+      <div class="message" @click="next" v-if="message">
+        <img class="img" src="./assets/images/empty.png" />
+        <img class="message-bg" src="./assets/images/message.png" />
+        <img class="breakglyph" src="./assets/images/breakglyph.png" />
+        <div class="message-box">{{ list[index] }}</div>
+      </div>
     </transition>
   </div>
 </template>
@@ -30,36 +36,80 @@
 <script setup>
 import { ref } from 'vue'
 
-const logo = ref(false)
-const title = ref(false)
+const bg = process.env.NODE_ENV === 'development' ? require('./assets/images/title_bg.png') : 'https://cdn.jsdelivr.net/gh/blacktunes/hiiropara@master/src/assets/images/title_bg.png'
+const 我打你啊 = new Audio(process.env.NODE_ENV === 'development' ? require('./assets/voices/我打你啊.mp3') : 'https://cdn.jsdelivr.net/gh/blacktunes/hiiropara@master/src/assets/voices/我打你啊.mp3')
+const 喵 = new Audio(process.env.NODE_ENV === 'development' ? require('./assets/voices/喵.mp3') : 'https://cdn.jsdelivr.net/gh/blacktunes/hiiropara@master/src/assets/voices/喵.mp3')
 
-const logoAudio = new Audio('https://cdn.jsdelivr.net/gh/blacktunes/hiiro-button@master/public/voices/%E5%96%B52.mp3')
-const audio = new Audio('https://cdn.jsdelivr.net/gh/blacktunes/hiiro-button@master/public/voices/%E6%88%91%E6%89%93%E4%BD%A0%E5%95%8A.mp3')
+const title = ref(true)
+const message = ref(false)
+const extra = ref(false)
+const tipShow = ref(false)
 
-logoAudio.oncanplay = () => {
-  logo.value = true
+const index = ref(0)
+const list = [
+  '我喜欢Hiiro',
+  '曾经在教室里登B站账号的时候被看见了收藏夹然后被嘲笑。有一点点难过，但我什么也没有说',
+  '只是心里有一个小小的声音坚定地重复，我喜欢Hiiro',
+  '我喜欢Hiiro',
+  '我喜欢Hiiro健康的笑声，喜欢她温柔地说晚上好，喜欢她打游戏时认真的样子，喜欢她说"我是你们的baba"时屑屑的表情，喜欢她轻轻在我耳边告诉我，"我打你们啊"',
+  '我知道我喜欢Hiiro',
+  '我会在听到她的声音的时候情不自禁地露出微笑，会在她说晚安的时候依依不舍地听那一个留给我的mua，会在她身体不好的时候不由自主地牵挂不已',
+  '她是901IQ的甜菜，是425kg的debiiro，是星辰般闪着光的点亮我的生命的Hiiro',
+  '我知道也许我的同学们说的是对的',
+  '我永远不可能真正遇见那只给我以温暖的粉色猫猫',
+  '我知道也许有一天她会不再出现，像这个世界上很多平凡的人一样，结婚生子，然后慢慢变老',
+  '我知道那句我永远跟你们也许终究只会是一个美好的谎言，有一天她会离去，而我再也找不到那个如她的人',
+  '但是我并不在乎。因为我喜欢hiiro',
+  '所以我不在乎她是谁、她在哪里、曾有着怎样的经历，我只是单纯地相信在这个世界上有一个我在乎的人，正在温柔地陪着我度过日出日落',
+  '就像我固执地相信她真的是九岁的901IQ的粉色猫猫一样',
+  '也许三分钟前路灯闪烁的灯光照出她的影子，也许三千里外她正吃着她喜欢的三文鱼，但那又有什么关系呢？',
+  '至少此刻她陪着我，告诉我我是她喜欢的王牛奶，告诉我这个世界上有个人在乎我',
+  '所以我会在听见那声remember me时红了眼眶',
+  '我们此生不会相见，但我认识她有趣而温柔的灵魂，拥抱着她带给我的感动与温存',
+  '也许在岁月的尽头，在时间失去意义的尽头，我们会变成了宇宙间最微小的尘土，相伴着化为永恒',
+  '而这便已足够',
+  '因为我喜欢Hiiro'
+]
+
+const showMsg = () => {
+  喵.play()
+  title.value = false
 }
 
-const playVoice = () => {
-  logoAudio.play()
-  logo.value = false
-}
-
-const showTitle = () => {
-  title.value = true
-}
-
-const show = ref(false)
-
-const click = () => {
-  audio.play()
-  audio.onended = () => {
-    show.value = true
+const next = () => {
+  if (++index.value >= list.length) {
+    message.value = false
+    extra.value = true
+    document.onkeydown = null
   }
 }
 
-const hide = () => {
-  show.value = false
+const titleLeave = () => {
+  message.value = true
+  document.onkeydown = ({ keyCode }) => {
+    if (keyCode === 13) {
+      next()
+    }
+  }
+}
+
+const messageLeave = () => {
+  title.value = true
+}
+
+const showTip = () => {
+  我打你啊.play()
+  我打你啊.onended = () => {
+    tipShow.value = true
+  }
+}
+
+const hideTip = () => {
+  tipShow.value = false
+}
+
+const exit = () => {
+  window.location.href = 'https://www.bilibili.com/video/BV1GJ411x7h7'
 }
 </script>
 
@@ -71,9 +121,15 @@ body
 img
   -webkit-user-drag none
 
+setTop(num)
+  top 23 + 13 * (num - 1) + '%'
+
 bg()
   background-repeat no-repeat
   background-size cover
+
+pointer()
+  cursor url('~./assets/static/cur_over.cur'), pointer
 
 .title
   position fixed
@@ -92,46 +148,68 @@ bg()
 
     .button
       position absolute
-      width 13%
-      height 11%
-      right 3.5%
+      width 16%
+      height 13%
+      right 1.5%
 
-    .button1
-      top 26.2%
-      background url('./assets/1.png')
+    .start
+      setTop(1)
+      background url('./assets/images/start.png')
       bg()
-      cursor pointer
+      pointer()
 
       &:hover
-        background url('./assets/1_hover.png')
+        background url('./assets/images/start_hover.png')
         bg()
 
-    .button2
-      top 38.4%
-      cursor not-allowed
-
-    .button3
-      top 50.8%
-      cursor not-allowed
-
-    .button4
-      top 63.4%
-      background url('./assets/4.png')
-      bg()
-      cursor pointer
-
-      &:hover
-        background url('./assets/4_hover.png')
+      &:active
+        background url('./assets/images/start_active.png')
         bg()
 
-    .button5
-      top 75.6%
-      background url('./assets/5.png')
+    .load
+      setTop(2)
+      background url('./assets/images/load.png')
       bg()
-      cursor pointer
+      cursor not-allowed
+
+    .config
+      setTop(3)
+      background url('./assets/images/config.png')
+      bg()
+      cursor not-allowed
+
+    .extra
+      setTop(4)
+      background url('./assets/images/extra.png')
+      bg()
+      pointer()
 
       &:hover
-        background url('./assets/5_hover.png')
+        background url('./assets/images/extra_hover.png')
+        bg()
+
+      &:active
+        background url('./assets/images/extra_active.png')
+        bg()
+
+    .extra-disable
+      setTop(4)
+      background url('./assets/images/extra_disable.png')
+      bg()
+      cursor not-allowed
+
+    .exit
+      setTop(5)
+      background url('./assets/images/exit.png')
+      bg()
+      pointer()
+
+      &:hover
+        background url('./assets/images/exit_hover.png')
+        bg()
+
+      &:active
+        background url('./assets/images/exit_active.png')
         bg()
 
     .tip
@@ -142,18 +220,36 @@ bg()
       max-width 100%
       max-height 100%
 
-.logo-wrapper
-  position fixed
-  inset 0
-  display flex
-  align-items center
-  justify-content center
-  pointer-events none
+  .message
+    position relative
+    background #fff
 
-  .logo
-    margin auto
-    max-width 100vw
-    max-height 100vh
+    .img
+      margin auto
+      max-width 100vw
+      max-height 100vh
+
+    .message-bg
+      position absolute
+      left 0
+      bottom 0
+      width 100%
+
+    .breakglyph
+      position absolute
+      right:10%
+      bottom 5%
+      width 2%
+      animation twinkle 0.5s alternate infinite
+
+    .message-box
+      position absolute
+      left 15%
+      right 15%
+      bottom 5%
+      top 80%
+      color #fff
+      text-shadow 0px 2px 2px rgba(0,0,0,0.7), 0 -2px 2px rgba(0,0,0,0.7), 2px 0 2px rgba(0,0,0,0.7), -2px 0 2px rgba(0,0,0,0.7)
 
 .fade-enter-active, .fade-leave-active
   transition opacity 0.3s
@@ -172,4 +268,11 @@ bg()
 
 .logo-enter-to, .logo-leave-from
   opacity 1
+
+@keyframes twinkle
+  from
+    opacity 1
+  to
+    opacity 0
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/assets/style/main.styl'
 
 createApp(App).mount('#app')
